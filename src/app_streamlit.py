ef6f6125b07ea8c8a76bacc0a110f80b999e6ab7
Victoria Guazzelli <166502413+VLGuazzelli@users.noreply.github.com>
2025-01-31 08:28:35 +0000

    cambiando ruta

--- src/app_streamlit.py
+++ src/app_streamlit.py
@@ -1,8 +1,8 @@
 from pickle import load
 import streamlit as st
 
-model = load(open("./src/decision_tree_classifier_default_42.sav", "rb"))
-scaler = load(open("./src/model_scaler.sav", "rb"))  # Asegúrate de haber guardado el scaler
+model = load(open("./decision_tree_classifier_default_42.sav", "rb"))
+scaler = load(open("./model_scaler.sav", "rb"))  # Asegúrate de haber guardado el scaler
 
 class_dict = {
     "0": "Sin Diabetes",
